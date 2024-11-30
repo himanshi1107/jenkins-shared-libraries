06268cc453da189ae0261bf271545b1dee84f1de
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2024-11-30 23:53:42 +0530

    Update dockerhub_push.groovy

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -5,6 +5,6 @@ def call(String Project, String ImageTag, String dockerHubUser){
                     passwordVariable:"dockerHubPass")]){
                  sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
                 }
-  sh "docker image tag ${Project}:${ImageTag} ${dockerHubUser}/${Project}:${ImageTag}""
+  sh "docker image tag ${Project}:${ImageTag} ${dockerHubUser}/${Project}:${ImageTag}"
   sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
